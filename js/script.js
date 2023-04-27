a7ffe4fec0484479cb947b0dcb1a07616f6888e0
Natania Rayos <aniarayos@gmail.com>
2023-04-27 10:13:39 +0800

    Initial

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const home = document.getElementById('home');
+const pldtPlan = document.getElementById('pldtPlan');
+const pldtEnterprise = document.getElementById('pldtEnterprise');
+const aboutUs = document.getElementById('aboutUs');
+const support = document.getElementById('support');
+const updates = document.getElementById('updates');
+const tracking = document.getElementById('tracking');
+// TODO:
+const contactUs = document.getElementById('contactUs');
+const termsConditions = document.getElementById('termsConditions');
+const securityPrivacy = document.getElementById('securityPrivacy');
+
+home.addEventListener('click', function() {
+     window.location.href = "index.html";
+  });
+pldtPlan.addEventListener('click', function() {
+//   window.location.href = "tracking.html";
+});
+pldtEnterprise.addEventListener('click', function() {
+    // window.location.href = "tracking.html";
+  });
+aboutUs.addEventListener('click', function() {
+//   window.location.href = "tracking.html";
+});
+support.addEventListener('click', function() {
+    // window.location.href = "tracking.html";
+  });
+updates.addEventListener('click', function() {
+//   window.location.href = "tracking.html";
+});
+
+tracking.addEventListener('click', function() {
+  window.location.href = "tracking.html";
+});
